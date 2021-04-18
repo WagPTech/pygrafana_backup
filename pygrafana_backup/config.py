b0bf99a9f1538118369d75e7b0f2f78aba271d67
wagnerportela <49325967+wagnerportela@users.noreply.github.com>
2021-04-18 10:22:24 -0300

    add address and api keys lab.

--- pygrafana_backup/config.py
+++ pygrafana_backup/config.py
@@ -1,6 +1,6 @@
 import os
 
-SERVER = os.environ['SERVER']
-API_KEY = os.environ['API_KEY']
+SERVER = os.environ['https://localhost:3000']
+API_KEY = os.environ['eyJrIjoiNTI0WEJzSmExWnk0cHRuTWNiTm5IVkt2bVk2cUNZZ1giLCJuIjoiQmFja3VwIiwiaWQiOjF9']
 HEADERS = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': f'Bearer {API_KEY}'}
 SSL_CHECK = False if os.environ.get('SSL_CHECK') is None else True
